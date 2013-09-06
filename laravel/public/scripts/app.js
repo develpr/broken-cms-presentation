1a1462a17f17961dfd63c3de7de6447c9b6c2f27
Kevin Mitchell <kevin@infielddesign.com>
2013-09-05 18:09:16 -0700

    more stuff

--- laravel/public/scripts/app.js
+++ laravel/public/scripts/app.js
@@ -2,8 +2,8 @@ $(document).foundation();
 
 var broken = {};
 
-broken.breakContent = function(){
-	$(".content.main .blocks").append('<div class="empty block row"><div class="moveme right"><i class="foundicon-up-arrow"></i><i class="foundicon-down-arrow"></i></div><div class="editable"></div></div>');
+broken.breakContent = function(locationClass){
+	$(".content."+locationClass+" .blocks").append('<div class="empty block row"><div class="moveme right"><i class="foundicon-up-arrow"></i><i class="foundicon-down-arrow"></i></div><div class="editable"></div></div>');
 	Aloha.jQuery('.editable').aloha();
 }
 
@@ -14,8 +14,9 @@ $(function(){
     });
 
 	$('.add-content').on('click', function(event){
+		location = $(this).attr('id');
 		event.preventDefault();
-		broken.breakContent();
+		broken.breakContent(location);
 	});
 	
 });
@@ -37,6 +38,9 @@ Aloha.bind('aloha-editable-activated', function (event, eventArgument) {
 Aloha.bind('aloha-editable-deactivated', function (event, eventArgument) {
 	$content = $(eventArgument.editable.obj[0])
 	$editing = $content.parent();
+	
+	console.log($editing.index());
+	console.log($editing.prev().index());
 
 	if($.trim($content.html()).length < 5){
 		$editing.addClass('empty');
